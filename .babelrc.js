5a720c1479e2769fb627a4d365d7bd65e66409ff
Juan Marchionatto <juan.r.marchio@gmail.com>
2020-04-01 10:40:23 -0400

    register possible plugin (which didn't work but could explore further)

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,10 @@
 // .babelrc.js
 module.exports = {
-  presets: ["@babel/react", "@babel/env"],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "babel-plugin-styled-components"
-  ]
+    presets: ['@babel/react', '@babel/env'],
+    plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-styled-components'],
+    // env: {
+    //     test: {
+    //         plugins: ['transform-es2015-modules-commonjs'],
+    //     },
+    // },
 };
